b263005ef59913fa908673cb8d633aefd233abe3
Nathaniel Wihardjo <nwihardjo@connect.ust.hk>
2019-06-05 23:43:44 +0800

    working dark theme

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from mainpage.models import Project, Technology
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+	pass
+
+class TechnologyAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Technology, TechnologyAdmin)
+
