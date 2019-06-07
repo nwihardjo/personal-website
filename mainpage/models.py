160d42466d3fa5ecea87472f52306451475c4069
Nathaniel Wihardjo <nwihardjo@connect.ust.hk>
2019-06-07 18:00:43 +0800

    updated cards and footer style

--- mainpage/models.py
+++ mainpage/models.py
@@ -4,11 +4,21 @@ from django.db import models
 class Technology(models.Model):
 	name = models.CharField(max_length=60)
 
+	def __str__(self):
+		return self.name
+
 class Project(models.Model):
 	title = models.CharField(max_length=100)
 	role = models.CharField(max_length=200)
 	description=models.TextField(blank=True)
 	image = models.FilePathField(path="/img")
-	start_date = models.DateField(auto_now=True)
-	end_date = models.DateField(auto_now=True)
+	start_date = models.DateField(auto_now=False)
+	end_date = models.DateField(auto_now=False)
 	technology = models.ManyToManyField(Technology)
+	url = models.URLField(max_length=200, blank=True)
+
+	class Meta:
+		ordering = ['-end_date__year', '-end_date__month']
+
+	def __str__(self):
+		return self.title
